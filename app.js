2af74a8f048c61f925c9526f28719394003f2952
unknown <briceballuais@gmail.com>
2020-10-27 21:03:53 +0100

    ajout de la connexion à la bdd, suppression du module de requêtes. config retourne désormais un objet.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//======================================================================================================= Express config
 
 var app = express();
 //enable ALL cors requests
@@ -25,13 +26,63 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
-// Apps's middlewares
 
-app.post('/test', (req, res, next) =>
-{
+//================================================================================================================== Data Base
+
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const config = require('./config');
+const uri = "mongodb+srv://admin:"+config.dbPassword+"@cluster0.86oxo.mongodb.net/"+config.dbName+"?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri, { useUnifiedTopology: true },
+  (err) => {
+    if (err) return console.error(err);
+    else console.log('Connected to Database')
+  },
+);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to database");
+
+    const db = client.db(config.dbName);
+// users CRUD
+    const users = db.collection('users')
+//C
+    app.post('/test', (req, res, next) => {
+      console.log(req.headers);
+})
+//R
+    app.get('/getUser',  async (req, res) => {
+        user = await users.findOne({name: "Bob"})
+        .catch((err) => {console.log(err)})
+        console.log(user)
+        res.send(user)
+        })
+//U
+//D
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close()
+    .catch((err)=>console.log(err))
+  }
+}
+// run
+run().catch(console.dir);
+
+//============================================================================================================= Middlewares
+
+/* app.post('/test', (req, res, next) => {
   console.log(req.body.message);
   console.log(req.headers);
   console.log(req.headers.authorization);
@@ -41,26 +92,8 @@ app.post('/test', (req, res, next) =>
 app.post('/', (req, res, next) => {
   console.log(req.body);
   next;
-})
+}) */
 
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//================================================================================================================ export
 
 module.exports = app
